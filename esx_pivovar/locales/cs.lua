d7af1fe679557b485b1e4074e077837a29de02b0
matuseksamuel <81881792+matuseksamuel@users.noreply.github.com>
2021-06-06 10:48:54 +0200

    Add files via upload

---
+++ esx_pivovar/locales/cs.lua
@@ -0,0 +1,46 @@
+-----------------------------------------
+--Made by samuel64#5943 from original: https://github.com/xSenSi/esx_vigneronjob
+-----------------------------------------
+Locales['cs'] = {
+  -- Obecné
+  ['cloakroom'] = 'šatna',
+  ['pivovar_clothes_civil'] = 'civilní oblek',
+  ['pivovar_clothes_pivovar'] = 'pivovarský outfit',
+  ['veh_menu'] = 'garáž',
+  ['not_enough_place'] = 'nemáš dostatek místa',
+  ['sale_in_prog'] = 'probíhá prodej...',
+  ['van'] = 'pracovní kamion',
+  ['open_menu'] = 'Otevři menu',
+  ['spawn_veh'] = 'stiskni ~INPUT_CONTEXT~ pro nasednutí do vozidla',
+  ['amount_invalid'] = 'neplatná částka',
+  ['press_to_open'] = 'stiskni ~INPUT_CONTEXT~ pro přístup do menu',
+  ['billing'] = 'fakturace',
+  ['invoice_amount'] = 'výše faktury',
+  ['no_players_near'] = 'žádní hráči poblíž',
+  ['store_veh'] = 'stikni ~INPUT_CONTEXT~ pro uchování vozidla',
+  ['comp_earned'] = 'vaše společnost vydělala ~g~$',
+  ['deposit_stock'] = 'vložit zásoby',
+  ['take_stock'] = 'vybrat zásoby',
+  ['boss_actions'] = 'akce šéfa',
+  ['quantity'] = 'množství',
+  ['quantity_invalid'] = 'neplatné množství',
+  ['inventory'] = 'inventář',
+  ['have_withdrawn'] = 'vybral jsi x',
+  ['added'] = 'přidal jsi x',
+
+  -- Pivovar
+  ['pivovar_client'] = 'Pivovar',
+  ['transforming_in_progress'] = 'Probíhá míchání chmelu... ',
+  ['chmel_taken'] = 'sbíráš chmel',
+  ['press_traitement'] = 'stiskni ~INPUT_CONTEXT~ pro balení piva',
+  ['press_collect'] = 'stiskni ~INPUT_CONTEXT~ pro sběr chmelu',
+  ['press_sell'] = 'stiskni ~INPUT_CONTEXT~ pro prodej produktu',
+  ['no_nealko_sale'] = 'nemáš dostatek chmelu na nelko',
+  ['no_radegast_sale'] = 'nemáš dostatek piva',
+  ['not_enough_chmel'] = 'nemáš dostatek chmelu',
+  ['plzen'] = '~g~úžasné, Pilsner Urquell !~w~',
+  ['no_product_sale'] = 'nemáš dostatek produktů',
+  ['press_traitement_nealko'] = 'stiskni ~INPUT_CONTEXT~ pro míchání chmelu',
+  ['used_nealko'] = 'vypil jsi Nealkoholické Pivo',
+  ['used_plzen'] = 'vypil jsi Pilsner Urquell',
+}
